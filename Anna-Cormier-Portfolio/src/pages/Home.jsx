e69352889285b5873aff7774974681bb95ea1b92
AnnaC0rmier <anna.cormier.96@gmail.com>
2024-03-05 09:35:47 -0600

    added quote to home page

--- Anna-Cormier-Portfolio/src/pages/Home.jsx
+++ Anna-Cormier-Portfolio/src/pages/Home.jsx
@@ -1,13 +1,20 @@
 import Grid from '@mui/joy/Grid';
 import img from '../assets/img/img.jpg';
+import CodeIcon from '@mui/icons-material/Code';
+import DataArrayIcon from '@mui/icons-material/DataArray';
+import DataObjectIcon from '@mui/icons-material/DataObject';
 
 export default function MyHome() {
     return (
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', marginTop:30 }}>
-            <Grid container sx={{ justifyContent: 'center', alignItems: 'center', height: '100%' }} rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', marginTop:30}}>
+            <Grid container sx={{  height: '100%' }} rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
                 <Grid xs={6} sx={{ textAlign: 'center' }}>
                     <h1 style={{color:'#da3d85'}}>ANNA CORMIER</h1>
                     <h2 style={{color:'#da3d85'}}>FULL STACK WEB DEVELOPER</h2>
+                    <div style={{color:'white' ,padding:100}}>
+                    <h1>"The best way to predict the future is to invent it."</h1>
+                    <p>- Alan Kay</p>
+                    </div>
                 </Grid>
                 <Grid xs={6} sx={{ textAlign: 'center' }}>
                     <img
